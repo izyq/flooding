b3317b0072be8163e0d9ccb1afa2ebf255fd1d99
izyq <izyq@qq.com>
2023-06-13 18:49:15 +0800

    fix:网关忽略数据库

--- waterflood-gateway-9001/src/main/java/club/cupk/waterflood/GatewayApplication.java
+++ waterflood-gateway-9001/src/main/java/club/cupk/waterflood/GatewayApplication.java
@@ -2,13 +2,14 @@ package club.cupk.waterflood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author wangdaff
  * @date 2023-06-13 09:53
  * @description
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
