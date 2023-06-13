5fb5d4935cad5b2c0e31f982dddd19b56093895c
izyq <izyq@qq.com>
2023-06-13 18:45:24 +0800

    feat:Filed cur没有d

--- waterflood-consumer-9004/src/main/java/club/cupk/waterflood/controller/FieldController.java
+++ waterflood-consumer-9004/src/main/java/club/cupk/waterflood/controller/FieldController.java
@@ -1,42 +1,58 @@
 package club.cupk.waterflood.controller;
 
 import club.cupk.waterflood.domain.Field;
+import club.cupk.waterflood.dto.filed.EditFieldDTO;
+import club.cupk.waterflood.dto.filed.FieldDTO;
 import club.cupk.waterflood.service.IFieldService;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import xin.altitude.cms.common.entity.AjaxResult;
 import xin.altitude.cms.common.entity.PageEntity;
 
 import java.util.Arrays;
 @RestController
-@RequestMapping("/waterflood/field")
+@RequestMapping("/asset/field/")
 public class FieldController{
     @DubboReference
     private IFieldService fieldService;
     @GetMapping("/page")
-    public AjaxResult page(PageEntity pageEntity,Field field){
-        return AjaxResult.success(fieldService.page(pageEntity.toPage(), Wrappers.lambdaQuery(field)));
+    public Object page(PageEntity pageEntity,String name){
+        return fieldService.page(pageEntity.toPage(), Wrappers.lambdaQuery(Field.builder().fieldName(name).build()));
     }
-    @GetMapping("/list")
-    public AjaxResult list(Field field){
-        return AjaxResult.success(fieldService.list(Wrappers.lambdaQuery(field)));
+    @PutMapping("/add")
+    public Object add(@RequestBody FieldDTO field) {
+
+        return fieldService.save(Field
+                .builder()
+                .fieldName(field.getName())
+                .fieldDesc(field.getDesc())
+                .fieldArea(field.getArea())
+                .fieldAddress(field.getAddress())
+                .build()
+        );
     }
-    @PostMapping("/push")
-    public AjaxResult add(@RequestBody Field field) {
-        return AjaxResult.success(fieldService.save(field));
+    @PostMapping("/edit")
+    public Object edit(@RequestBody EditFieldDTO field) {
+        return fieldService.updateById(Field
+                .builder()
+                .fieldId(field.getId())
+                .fieldName(field.getName())
+                .fieldDesc(field.getDesc())
+                .fieldArea(field.getArea())
+                .fieldAddress(field.getAddress())
+                .build());
     }
-    @PutMapping("/edit")
-    public AjaxResult edit(@RequestBody Field field) {
-        return AjaxResult.success(fieldService.updateById(field));
-    }
-    @DeleteMapping("/delete/{fieldIds}")
-    public AjaxResult delete(@PathVariable Long[] fieldIds) {
-        return AjaxResult.success(fieldService.removeByIds(Arrays.asList(fieldIds)));
+    @DeleteMapping("/delete/{id}")
+    public Object delete(@PathVariable Long fieldId) {
+        return fieldService.removeById(fieldId);
     }
     @GetMapping(value = "/detail/{fieldId}")
     public AjaxResult detail(@PathVariable("fieldId") Long fieldId) {
         return AjaxResult.success(fieldService.getById(fieldId));
+    }
+    @GetMapping("/list")
+    public AjaxResult list(Field field){
+        return AjaxResult.success(fieldService.list(Wrappers.lambdaQuery(field)));
     }
 }
