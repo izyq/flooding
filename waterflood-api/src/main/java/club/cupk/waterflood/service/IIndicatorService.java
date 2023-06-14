e3b6c273490bbaebb65c5c6b98e9c07149618168
izyq <izyq@qq.com>
2023-06-14 22:21:53 +0800

    feat:基础业务

--- waterflood-api/src/main/java/club/cupk/waterflood/service/IIndicatorService.java
+++ waterflood-api/src/main/java/club/cupk/waterflood/service/IIndicatorService.java
@@ -2,6 +2,7 @@ package club.cupk.waterflood.service;
 import club.cupk.waterflood.domain.CustEvaluation;
 import club.cupk.waterflood.domain.Indicator;
 import club.cupk.waterflood.entity.vo.IndicatorVo;
+import club.cupk.waterflood.util.DubboBean;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -15,33 +16,33 @@ import xin.altitude.cms.common.util.EntityUtils;
 import xin.altitude.cms.common.util.SpringUtils;
 
 public interface IIndicatorService extends IService<Indicator>{
-//    /** 通过方法调用来解决循环依赖问题 */
-//    default ICustEvaluationService getCustEvaluationService() {
-//        return SpringUtils.getBean(ICustEvaluationService.class);
-//    }
-//    /**查询用户实体类Vo*/
-//    default IndicatorVo getOneVo(Long indicatorId) {
-//        IndicatorVo indicatorVo = EntityUtils.toObj(getById(indicatorId), IndicatorVo::new);
-//        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).eq(CustEvaluation::getIndicatorId, indicatorVo.getIndicatorId()));
-//        indicatorVo.setCustEvaluationList(custEvaluationList);
-//        return indicatorVo;
-//    }
-//    /**查询实体类Vo列表*/
-//    default List<IndicatorVo> listVo(Indicator indicator) {
-//        List<IndicatorVo> indicatorVoList = EntityUtils.toList(list(Wrappers.lambdaQuery(indicator)), IndicatorVo::new);
-//        Set<Long> indicatorIds = EntityUtils.toSet(indicatorVoList, IndicatorVo::getIndicatorId);
-//        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).in(CustEvaluation::getIndicatorId, indicatorIds));
-//        Map<Long, List<CustEvaluation>> map = custEvaluationList.stream().collect(Collectors.groupingBy(CustEvaluation::getIndicatorId));
-//        indicatorVoList.forEach(e -> e.setCustEvaluationList(map.get(e.getIndicatorId())));
-//        return indicatorVoList;
-//    }
-//    /**分页查询实体类Vo*/
-//    default IPage<IndicatorVo> pageVo(IPage<Indicator> page, Indicator indicator) {
-//        IPage<IndicatorVo> indicatorVoPage = EntityUtils.toPage(page(page, Wrappers.lambdaQuery(indicator)), IndicatorVo::new);
-//        Set<Long> indicatorIds = EntityUtils.toSet(indicatorVoPage.getRecords(), IndicatorVo::getIndicatorId);
-//        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).in(CustEvaluation::getIndicatorId, indicatorIds));
-//        Map<Long, List<CustEvaluation>> map = custEvaluationList.stream().collect(Collectors.groupingBy(CustEvaluation::getIndicatorId));
-//        indicatorVoPage.getRecords().forEach(e -> e.setCustEvaluationList(map.get(e.getIndicatorId())));
-//        return indicatorVoPage;
-//    }
+    /** 通过方法调用来解决循环依赖问题 */
+    default ICustEvaluationService getCustEvaluationService() {
+        return DubboBean.getDubboBean(ICustEvaluationService.class);
+    }
+    /**查询用户实体类Vo*/
+    default IndicatorVo getOneVo(Long indicatorId) {
+        IndicatorVo indicatorVo = EntityUtils.toObj(getById(indicatorId), IndicatorVo::new);
+        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).eq(CustEvaluation::getIndicatorId, indicatorVo.getIndicatorId()));
+        indicatorVo.setCustEvaluationList(custEvaluationList);
+        return indicatorVo;
+    }
+    /**查询实体类Vo列表*/
+    default List<IndicatorVo> listVo(Indicator indicator) {
+        List<IndicatorVo> indicatorVoList = EntityUtils.toList(list(Wrappers.lambdaQuery(indicator)), IndicatorVo::new);
+        Set<Long> indicatorIds = EntityUtils.toSet(indicatorVoList, IndicatorVo::getIndicatorId);
+        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).in(CustEvaluation::getIndicatorId, indicatorIds));
+        Map<Long, List<CustEvaluation>> map = custEvaluationList.stream().collect(Collectors.groupingBy(CustEvaluation::getIndicatorId));
+        indicatorVoList.forEach(e -> e.setCustEvaluationList(map.get(e.getIndicatorId())));
+        return indicatorVoList;
+    }
+    /**分页查询实体类Vo*/
+    default IPage<IndicatorVo> pageVo(IPage<Indicator> page, Indicator indicator) {
+        IPage<IndicatorVo> indicatorVoPage = EntityUtils.toPage(page(page, Wrappers.lambdaQuery(indicator)), IndicatorVo::new);
+        Set<Long> indicatorIds = EntityUtils.toSet(indicatorVoPage.getRecords(), IndicatorVo::getIndicatorId);
+        List<CustEvaluation> custEvaluationList = getCustEvaluationService().list(Wrappers.lambdaQuery(CustEvaluation.class).in(CustEvaluation::getIndicatorId, indicatorIds));
+        Map<Long, List<CustEvaluation>> map = custEvaluationList.stream().collect(Collectors.groupingBy(CustEvaluation::getIndicatorId));
+        indicatorVoPage.getRecords().forEach(e -> e.setCustEvaluationList(map.get(e.getIndicatorId())));
+        return indicatorVoPage;
+    }
 }
