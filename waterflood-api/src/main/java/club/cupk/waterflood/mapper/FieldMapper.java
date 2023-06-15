5afec3ea26a6278d56ae4c920f7c9abac9a4dbf9
izyq <izyq@qq.com>
2023-06-15 13:34:06 +0800

    feat:添加Mapper

--- waterflood-api/src/main/java/club/cupk/waterflood/mapper/FieldMapper.java
+++ waterflood-api/src/main/java/club/cupk/waterflood/mapper/FieldMapper.java
@@ -3,7 +3,11 @@ import club.cupk.waterflood.domain.Field;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 @Mapper
 @Transactional(rollbackFor = Exception.class)
 public interface FieldMapper extends BaseMapper<Field>{
+    List<Field> findAllFields();
 }
