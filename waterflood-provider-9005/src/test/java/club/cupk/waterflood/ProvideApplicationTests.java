fafa9844e6118e01373325f207817bdb62b448de
izyq <izyq@qq.com>
2023-06-15 13:41:41 +0800

    feat:优化import

--- waterflood-provider-9005/src/test/java/club/cupk/waterflood/ProvideApplicationTests.java
+++ waterflood-provider-9005/src/test/java/club/cupk/waterflood/ProvideApplicationTests.java
@@ -1,8 +1,6 @@
 package club.cupk.waterflood;
 
-import club.cupk.waterflood.domain.Field;
 import club.cupk.waterflood.mapper.FieldMapper;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ServiceProviderApplication.class)
