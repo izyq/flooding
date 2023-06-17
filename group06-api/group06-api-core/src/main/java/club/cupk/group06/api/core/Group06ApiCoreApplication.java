79c84e3c2a4c06a77dfb3d7ccbeab41b5636f1b7
Icy <icy_official@qq.com>
2023-06-17 21:14:29 +0800

    build: 依赖修改

--- group06-api/group06-api-core/src/main/java/club/cupk/group06/api/core/Group06ApiCoreApplication.java
+++ group06-api/group06-api-core/src/main/java/club/cupk/group06/api/core/Group06ApiCoreApplication.java
@@ -1,9 +1,13 @@
 package club.cupk.group06.api.core;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan(value = "club.cupk.group06.data.core.mapper")
+@ComponentScan(value = {"club.cupk.group06.api.core", "club.cupk.group06.common.core"})
 public class Group06ApiCoreApplication {
 
     public static void main(String[] args) {
