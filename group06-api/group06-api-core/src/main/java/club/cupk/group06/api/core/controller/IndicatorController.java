ff5f5a4b0532a88b8f4a3c78787a7c2063d71c60
Icy <icy_official@qq.com>
2023-06-18 22:44:00 +0800

    feat: IndicatorController

--- group06-api/group06-api-core/src/main/java/club/cupk/group06/api/core/controller/IndicatorController.java
+++ group06-api/group06-api-core/src/main/java/club/cupk/group06/api/core/controller/IndicatorController.java
@@ -17,11 +17,11 @@ public class IndicatorController {
     private IndicatorService indicatorService;
 
     @GetMapping("/page")
-    public AjaxResult pageName(PageEntity pageEntity, String name) {
-        if(name==null){
-            return AjaxResult.success(page(pageEntity,null));
+    public AjaxResult pageName(PageEntity pageEntity, String fieldName, String floodingPlan) {
+        if (fieldName == null) {
+            return AjaxResult.success(page(pageEntity, null));
         }
-        return indicatorService.getIndicatorByName(pageEntity.toPage(), Indicator.builder().indicatorName(name).build());
+        return indicatorService.getIndicatorByName(pageEntity.toPage(), Indicator.builder().indicatorName(fieldName).build());
     }
 
     @GetMapping("/pageAll")
