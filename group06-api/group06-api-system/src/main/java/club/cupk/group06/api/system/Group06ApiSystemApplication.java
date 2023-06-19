bba85d169cdf945b01f30be3669d210fb3868360
izyq <izyq@qq.com>
2023-06-19 23:48:59 +0800

    fix:移除EnableDiscoveryClient

--- group06-api/group06-api-system/src/main/java/club/cupk/group06/api/system/Group06ApiSystemApplication.java
+++ group06-api/group06-api-system/src/main/java/club/cupk/group06/api/system/Group06ApiSystemApplication.java
@@ -3,10 +3,8 @@ package club.cupk.group06.api.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @MapperScan(value = "club.cupk.group06.data.system.mapper")
 public class Group06ApiSystemApplication {
 
